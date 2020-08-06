fd19b67e708829eab581ce6888d6ade546595560
demon <656102866@qq.com>
2020-08-06 15:23:45 +0800

    first commit

---
+++ my_blog/settings/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/8/6 15:21
+# @Author  : Demon
+# @File    : __init__.py.py
---
+++ my_blog/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for my_blog project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+profile = os.environ.get('PROJECT_PROFILE', 'develop')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_blog.settings.%s" % profile)
+
+application = get_wsgi_application()
